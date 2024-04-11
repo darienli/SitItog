0e3f392cc5ccc499ffe20577abb734e1278291ef
Daria Khusainova <dashahusainova@bk.ru>
2024-04-11 17:18:41 +0400

    Changed to Fibonacci that starts from 0, 1,...

--- main.cpp
+++ main.cpp
@@ -5,16 +5,11 @@ using namespace std;
 int fib(int n){
     if (n == 0) return 0;
     if (n == 1 || n == 2) return 1;
-    return fib(n-1) + fib(n-2);
+    return fib(n - 1) + fib(n - 2);
 }
 
 int main(){
     int n; cin >> n;
-    int ans = fib(n);
+    int ans = fib(n-1);
     cout << ans;
 }
-
-
-
-
-
