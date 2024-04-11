68b8f27b40a0fd3df7ba7fd6c16016906a4745f6
Daria Khusainova <dashahusainova@bk.ru>
2024-04-11 17:34:40 +0400

    Changed to iterative way to create the Fibonacci sequence that starts from 0,1,...

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 
 using namespace std;
 
-int fib(int n){
-    if (n == 0) return 0;
-    if (n == 1 || n == 2) return 1;
-    return fib(n - 1) + fib(n - 2);
+void fib(int n){
+    int previous_el = 1; int current_el = 1;
+    if (n > 1) {
+        cout << "0"<<"\n"<< "1"<<"\n";
+        for (int k = 2; k < n; ++k){
+            cout << current_el << "\n";
+            current_el +=previous_el;
+            previous_el = current_el - previous_el;
+        }
+    }
+    else cout<<"0";
 }
 
 int main(){
-    int n; cin >> n;
-    int ans = fib(n-1);
-    cout << ans;
+    int n;
+    cout << "n: "; cin >> n;
+    fib(n);
 }
