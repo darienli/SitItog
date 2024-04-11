aa40e54be319c8afe2ba6e783ecdfc384bf1c8d1
Daria Khusainova <dashahusainova@bk.ru>
2024-04-11 17:15:34 +0400

    Create the Fibonacci sequence that starts from 1, 1,... by the recursive way

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
-int main(){
-    cout << "Hello, World!";
+int fib(int n){
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    return fib(n-1) + fib(n-2);
 }
+
+int main(){
+    int n; cin >> n;
+    int ans = fib(n);
+    cout << ans;
+}
+
+
+
+
+
